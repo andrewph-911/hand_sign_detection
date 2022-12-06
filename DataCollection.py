3a3fea2e982840726403e65243afad6aaef12a55
Andrew Phạm <96015705+andrewph-911@users.noreply.github.com>
2022-12-06 22:15:46 +0700

    Add files via upload

--- DataCollection.py
+++ DataCollection.py
@@ -5,50 +5,83 @@ import math
 import time
 
 cap = cv2.VideoCapture(0)
-detector = HandDetector(maxHands=1)
+detector: HandDetector = HandDetector(detectionCon=0.8, maxHands=2)
 
 offset = 20
 imgSize = 300
 
-folder = "DataImages/OKAY"
+folder = "DataImages/Test"
 counter = 0
 
 while True:
     success, img = cap.read()
     hands, img = detector.findHands(img)
-    if hands:
-        hand = hands[0]
-        x, y, w, h = hand['bbox']
+    if hands:   # hand 1
+        hand1 = hands[0]
+        lmList = hand1['lmList']    # list of 21 Landmarks points
+        x, y, w, h = hand1['bbox']  # Bounding Box info x,y,w,h
 
-        imgWhite = np.ones((imgSize, imgSize, 3), np.uint8) * 255
-        imgCrop = img[y - offset:y + h + offset, x - offset:x + w + offset]
+        imgWhite1 = np.ones((imgSize, imgSize, 3), np.uint8) * 255
+        imgCrop1 = img[y - offset:y + h + offset, x - offset:x + w + offset]
 
-        imgCropShape = imgCrop.shape
+        imgCropShape1 = imgCrop1.shape
 
         aspectRatio = h / w
 
         if aspectRatio > 1:
             k = imgSize / h
             wCal = math.ceil(k * w)
-            imgResize = cv2.resize(imgCrop, (wCal, imgSize))
-            imgResizeShape = imgResize.shape
+            imgResize1 = cv2.resize(imgCrop1, (wCal, imgSize))
+            imgResizeShap1 = imgResize1.shape
             wGap = math.ceil((imgSize - wCal) / 2)
-            imgWhite[:, wGap:wCal + wGap] = imgResize
+            imgWhite1[:, wGap:wCal + wGap] = imgResize1
 
         else:
             k = imgSize / w
             hCal = math.ceil(k * h)
-            imgResize = cv2.resize(imgCrop, (imgSize, hCal))
-            imgResizeShape = imgResize.shape
+            imgResize1 = cv2.resize(imgCrop1, (imgSize, hCal))
+            imgResizeShape1 = imgResize1.shape
             hGap = math.ceil((imgSize - hCal) / 2)
-            imgWhite[hGap:hCal + hGap, :] = imgResize
+            imgWhite1[hGap:hCal + hGap, :] = imgResize1
 
-        cv2.imshow("ImageCrop", imgCrop)
-        cv2.imshow("ImageWhite", imgWhite)
+        cv2.imshow("ImageCrop1", imgCrop1)
+        cv2.imshow("ImageWhite1", imgWhite1)
+
+    if len(hands) == 2:     # hand 2
+        hand2 = hands[1]    # list of 21 Landmarks points
+        lmList = hand2['lmList']    # Bounding Box info x,y,w,h
+        x, y, w, h = hand2['bbox']
+
+        imgWhite2 = np.ones((imgSize, imgSize, 3), np.uint8) * 255
+        imgCrop2 = img[y - offset:y + h + offset, x - offset:x + w + offset]
+
+        imgCropShape2 = imgCrop2.shape
+
+        aspectRatio = h / w
+
+        if aspectRatio > 1:
+            k = imgSize / h
+            wCal = math.ceil(k * w)
+            imgResize2 = cv2.resize(imgCrop2, (wCal, imgSize))
+            imgResizeShap2 = imgResize2.shape
+            wGap = math.ceil((imgSize - wCal) / 2)
+            imgWhite2[:, wGap:wCal + wGap] = imgResize2
+
+        else:
+            k = imgSize / w
+            hCal = math.ceil(k * h)
+            imgResize2 = cv2.resize(imgCrop2, (imgSize, hCal))
+            imgResizeShape2 = imgResize2.shape
+            hGap = math.ceil((imgSize - hCal) / 2)
+            imgWhite2[hGap:hCal + hGap, :] = imgResize2
+
+        cv2.imshow("ImageCrop2", imgCrop2)
+        cv2.imshow("ImageWhite2", imgWhite2)
 
     cv2.imshow("Image", img)
     key = cv2.waitKey(1)
     if key == ord("s"):
         counter += 1
-        cv2.imwrite(f'{folder}/Image_{time.time()}.jpg',imgWhite)
+        cv2.imwrite(f'{folder}/Image_{time.time()}.jpg', imgWhite1)
+        cv2.imwrite(f'{folder}/Image_{time.time()}.jpg', imgWhite2)
         print(counter)
